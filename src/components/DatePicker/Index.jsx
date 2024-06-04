edeb8434bbd4bde13819a96f77f2a87b8e880feb
AqibNiazi <aqibjaved5201@gmail.com>
2024-06-04 18:13:14 +0500

    date picker add

--- src/components/DatePicker/Index.jsx
+++ src/components/DatePicker/Index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { MdDateRange } from "react-icons/md";
+const CustomDatePicker = ({ label }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
 
-const DatePicker = ({ label }) => {
   return (
     <div className="w-full">
       <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">
@@ -8,25 +12,17 @@ const DatePicker = ({ label }) => {
       </label>
       <div className="relative ">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
+          <MdDateRange className="text-black" />
         </div>
-        <input
-          datepicker
-          type="text"
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Select date"
+          placeholderText="Select date"
         />
       </div>
     </div>
   );
 };
 
-export default DatePicker;
+export default CustomDatePicker;
