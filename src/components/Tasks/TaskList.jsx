5610d9e00f408dfdad1cc6ae63b4667055bd6c49
abasitaccrual <a.basit@accrualhub.com>
2024-06-24 13:14:37 +0500

    working on task feature

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import TaskItem from "./TaskItem";
 
-export default function TaskList() {
+export default function TaskList({ tasks, onToggleCompleted, onDeleteTask, onEditTask }) {
   return (
-    <ul>
-      <li>List</li>
+    <ul className="flex flex-col gap-2 mt-4">
+      {tasks.map((task) => (
+        <TaskItem
+          task={task}
+          onToggleCompleted={onToggleCompleted}
+          onDeleteTask={onDeleteTask}
+          onEditTask={onEditTask}
+        />
+      ))}
     </ul>
   );
 }
