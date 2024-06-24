84a2e194f78adc8dc0daf0e6d2453fd8c1f2eccb
abasitaccrual <a.basit@accrualhub.com>
2024-06-24 10:37:28 +0500

    Working on task feature

--- src/components/Tasks/CreateTask.jsx
+++ src/components/Tasks/CreateTask.jsx
@@ -1,11 +1,18 @@
 import { InputContainer, Input } from "@components";
+import { useState } from "react";
 
 function CreateTask() {
+  const [task, setTask] = useState("");
+  const handleSubmit = function (e) {
+    e.preventDefault();
+  };
   return (
     <div>
-      <InputContainer>
-        <Input />
-      </InputContainer>
+      <form onSubmit={handleSubmit}>
+        <InputContainer>
+          <Input />
+        </InputContainer>
+      </form>
     </div>
   );
 }
--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,6 +1,14 @@
 import { CreateTask, EmptyTasks } from ".";
 
+const initalTasks = [
+  {
+    title: "Pitty Pat",
+    isCompleted: false,
+  },
+];
+
 export default function Task() {
+  const [tasks, setTasks] = useState(initalTasks);
   return (
     <div className="divide divide-y space-y-5">
       <EmptyTasks />
