d2928b6fe2805b53f5c4c395897447d2191e2dec
abasitaccrual <a.basit@accrualhub.com>
2024-07-04 15:01:30 +0500

    issue of redirection resolved

--- src/components/Forms/Signatures/index.jsx
+++ src/components/Forms/Signatures/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Input, InputContainer, CustomDatePicker } from "@components";
-const SignaturesForm = ({ className }) => {
+const SignaturesForm = ({ className, register }) => {
   return (
     <div className={`w-full ${className}`}>
       <InputContainer className="flex flex-col md:flex-row justify-between md:mb-4">
         <Input
-          label="Customer Signature:"
+          label="Company Signature:"
           placeholder="john Snow"
           type="text"
           applyMarginBottom={true}
           className="md:mr-4  mb-4 md:mb-0"
+          name="company_signature"
+          register={register}
         />
         <Input
           label="Printed Name:"
@@ -17,6 +19,8 @@ const SignaturesForm = ({ className }) => {
           type="text"
           applyMarginBottom={true}
           className="md:mr-4  mb-4 md:mb-0"
+          name="company_printed_name"
+          register={register}
         />
 
         <CustomDatePicker label="Select a Date" className="mb-4 md:mb-0" />
@@ -28,6 +32,8 @@ const SignaturesForm = ({ className }) => {
           type="text"
           applyMarginBottom={true}
           className="md:mr-4  mb-4 md:mb-0"
+          name="customer_signature"
+          register={register}
         />
         <Input
           label="Printed Name:"
@@ -35,6 +41,8 @@ const SignaturesForm = ({ className }) => {
           type="text"
           applyMarginBottom={true}
           className="md:mr-4  mb-4 md:mb-0"
+          name="customer_printed_name"
+          register={register}
         />
 
         <CustomDatePicker label="Select a Date" className="mb-4 md:mb-0" />
