d66a89a15843fa7c17c9b16aabb226e1de4ace4c
AqibNiazi <aqibjaved5201@gmail.com>
2024-07-25 19:18:25 +0500

    optimized components rerendering with memo

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import JobsReducer from "./slices/JobsSlice";
 import customerReducer from "./slices/customerSlice";
 import registerReducer from "./slices/registerSlice";
+import activeTabReducer from "./slices/activeTabSlice";
 export const store = configureStore({
   reducer: {
     jobs: JobsReducer,
     customer: customerReducer,
     register: registerReducer,
+    activeTab: activeTabReducer,
   },
 });
