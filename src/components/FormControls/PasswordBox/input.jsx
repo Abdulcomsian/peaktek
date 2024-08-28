22d0396647383e2e1faabcab296e25c0322f4ffb
abasitaccrual <a.basit@accrualhub.com>
2024-08-28 15:26:24 +0500

    some minor UI changes done

--- src/components/FormControls/PasswordBox/input.jsx
+++ src/components/FormControls/PasswordBox/input.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import { Input } from "antd";
+import { Controller } from "react-hook-form";
 
 const PasswordBox = ({
-  className,
-  disabled,
+  control,
+  name,
   label,
   placeholder,
   id,
-  value,
-  onBlur,
-  onChange,
-  name,
   error,
   touched,
+  disabled,
+  className,
+  rules, // added for validation rules
 }) => {
   return (
     <div className={`w-full ${className}`}>
@@ -25,27 +25,33 @@ const PasswordBox = ({
           {label}
         </label>
       )}
-      <Input.Password
-        size="large"
-        iconRender={(visible) =>
-          visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-        }
-        className={`${
-          error && touched ? "border border-red-600" : "border border-gray-300"
-        } bg-gray-50 text-gray-900 text-sm rounded-md w-full p-2.5 focus:outline-none ${
-          error && touched
-            ? "focus:border-red-600 focus:ring-0 outline-none "
-            : "focus:border-blue-500 hover:bg-white"
-        }`}
-        id={id}
-        placeholder={placeholder}
+      <Controller
         name={name}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        disabled={disabled}
+        control={control}
+        rules={rules}
+        render={({ field }) => (
+          <Input.Password
+            size="large"
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+            className={`${
+              error && touched
+                ? "border border-red-600"
+                : "border border-gray-300"
+            } bg-gray-50 text-gray-900 text-sm rounded-md w-full p-2.5 focus:outline-none ${
+              error && touched
+                ? "focus:border-red-600 focus:ring-0 outline-none "
+                : "focus:border-blue-500 hover:bg-white"
+            }`}
+            id={id}
+            placeholder={placeholder}
+            disabled={disabled}
+            {...field} // spread field props to the Input.Password component
+          />
+        )}
       />
-      {error && touched && <p className="text-red-600 text-sm mt-1">{error}</p>}
+      {error && <p className="text-red-600 text-sm mt-1">{error}</p>}
     </div>
   );
 };
